f337eee8bbd7d1686054223bf34fc2ef2658c57e
Muhammad Shaheer Zafar <shaheerzafar592@gmail.com>
2026-07-07 16:08:42 +0500

    server is updaed just to check docker is working fine or not

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ const app = require('./app');
 const port = 3000;
 app.listen(port, '0.0.0.0', () => {
   //eslint-disable-next-line no-console
-  console.log(`Server listening on port ${port}`);
+  console.log(`Server listening on ${port}`);
 });
